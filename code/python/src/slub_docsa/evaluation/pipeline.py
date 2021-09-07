34d5fbff9c59092865908cac5283147518fbee70
Thomas Low <code@thomaslow.com>
2021-09-07 07:15:03 +0000

    Calculate subject order list for whole data set in case rare subjects are assigned to either training or test set, but not both.

--- code/python/src/slub_docsa/evaluation/pipeline.py
+++ code/python/src/slub_docsa/evaluation/pipeline.py
@@ -27,18 +27,24 @@ def evaluate_dataset(
     score_matrix = np.empty((len(models), len(score_functions), n_splits))
     score_matrix[:, :, :] = np.NaN
 
+    subject_list = unique_subject_list(dataset.subjects)
+
+    logger.info("prepare cross validation splits")
     for i, split in enumerate(cross_validation_split(n_splits, dataset, random_state=random_state)):
         train_dataset, test_dataset = split
-        train_subject_list = unique_subject_list(train_dataset.subjects)
-        train_incidence_matrix = subject_incidence_matrix_from_list(train_dataset.subjects, train_subject_list)
-        test_incidence_matrix = subject_incidence_matrix_from_list(test_dataset.subjects, train_subject_list)
+        train_incidence_matrix = subject_incidence_matrix_from_list(train_dataset.subjects, subject_list)
+        test_incidence_matrix = subject_incidence_matrix_from_list(test_dataset.subjects, subject_list)
 
         for j, model in enumerate(models):
-            logger.info("train %s for %d-th split", str(model), i + 1)
+            logger.info("evaluate model %s for %d-th split", str(model), i + 1)
+            logger.info("do training")
             model.fit(train_dataset.documents, train_incidence_matrix)
+            logger.info("do prediction")
             predicted_subject_probabilities = model.predict_proba(test_dataset.documents)
-
+            logger.info("do scoring")
             for k, score_function in enumerate(score_functions):
                 score_matrix[j, k, i] = score_function(test_incidence_matrix, predicted_subject_probabilities)
+
+            logger.info("scores are: %s", str(score_matrix[j, :, i]))
 
     return score_matrix
