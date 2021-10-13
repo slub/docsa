1f83b9549469e2c9344442cadcd5010f9715f732
Thomas Low <code@thomaslow.com>
2021-10-13 09:33:14 +0000

    Limit number of charaters used from fulltext of documents for reasonable training and prediction times.

--- code/python/src/slub_docsa/data/preprocess/document.py
+++ code/python/src/slub_docsa/data/preprocess/document.py
@@ -7,5 +7,7 @@ def document_as_concatenated_string(doc: Document) -> str:
     """Convert a document to a string by simple concatenation."""
     text = doc.title
     if doc.abstract is not None:
-        text += " " + doc.abstract
+        text += "\n" + doc.abstract
+    if doc.fulltext is not None:
+        text += "\n" + doc.fulltext[:1000]
     return text
