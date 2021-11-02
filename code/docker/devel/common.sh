1207c5b471a722706c1acfb86c28df1c562ab561
Thomas Low <code@thomaslow.com>
2021-11-02 17:19:14 +0100

    Remove tensorflow as dependency and build from nvidia cuda image directly instead, since tensorflow is not used (yet).

--- code/docker/devel/common.sh
+++ code/docker/devel/common.sh
@@ -6,17 +6,17 @@ export COMPOSE_CMD="podman-compose"
 # export COMPOSE_CMD="docker-compose"
 
 if [ "$GPU_MODE" == "gpu" ]; then
-    TENSORFLOW_IMAGE_TAG="2.6.0-gpu"
+    BASE_IMAGE="nvidia/cuda:11.4.2-cudnn8-runtime-ubuntu18.04"
 fi
 
 if [ "$GPU_MODE" == "cpu" ]; then
-    TENSORFLOW_IMAGE_TAG="2.6.0"
+    BASE_IMAGE="python:3.6-slim"
 fi
 
-if [ -z "$TENSORFLOW_IMAGE_TAG" ]; then
+if [ -z "$BASE_IMAGE" ]; then
     echo "You need to provide build mode as argument: either 'cpu' or 'gpu'."
     exit 1
 fi
 
-export TENSORFLOW_IMAGE_TAG=${TENSORFLOW_IMAGE_TAG}
+export BASE_IMAGE=${BASE_IMAGE}
 export PROJECT_NAME="slub_docsa_${GPU_MODE}"
