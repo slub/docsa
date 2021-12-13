a160a736986afba30646192c0e7b365f2f5755b4
Thomas Low <code@thomaslow.com>
2021-12-13 13:00:15 +0000

    Update package script.

--- code/python/clean.sh
+++ code/python/clean.sh
@@ -2,5 +2,5 @@
 
 rm -rf build
 rm -rf dist
-rm -rf slub_docsa.egg-info
+rm -rf src/slub_docsa.egg-info
 rm -rf .pytest_cache
--- code/python/package_test.sh
+++ code/python/package_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # remove any existing installed packages
-python3 -m pip uninstall slub_docsa
+python3 -m pip uninstall -y slub_docsa
 
 # build package
 bash ./package_build.sh
@@ -13,5 +13,5 @@ python3 -m pip install dist/*.whl
 bash ./test_run.sh
 
 # reinstall developer version
-python3 -m pip uninstall slub_docsa
+python3 -m pip uninstall -y slub_docsa
 python3 -m pip install -e .[dev,test]
