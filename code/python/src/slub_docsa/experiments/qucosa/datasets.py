94d1534beefbcd9966530f4635a4b162cf66d8ce
Thomas Low <code@thomaslow.com>
2022-01-07 10:51:41 +0000

    Add qucosa dataset variants for testing level based pruning.

--- code/python/src/slub_docsa/experiments/qucosa/datasets.py
+++ code/python/src/slub_docsa/experiments/qucosa/datasets.py
@@ -15,7 +15,6 @@ from slub_docsa.common.subject import SubjectHierarchy
 from slub_docsa.data.load.qucosa import qucosa_subject_hierarchy_by_subject_schema, read_qucosa_samples
 from slub_docsa.data.load.qucosa import read_qucosa_documents_from_directory
 from slub_docsa.data.preprocess.dataset import filter_subjects_with_insufficient_samples
-from slub_docsa.data.preprocess.document import apply_nltk_snowball_stemming_to_document_samples_iterator
 from slub_docsa.data.preprocess.language import filter_samples_by_detected_language_via_langid
 from slub_docsa.data.preprocess.subject import prune_subject_targets_to_level, prune_subject_targets_to_minimum_samples
 from slub_docsa.data.store.dataset import load_persisted_dataset_from_lazy_sample_iterator
@@ -24,34 +23,33 @@ from slub_docsa.evaluation.incidence import unique_subject_order
 logger = logging.getLogger(__name__)
 
 
-def _prune_hierarchical_min_samples(samples_iterator, min_samples, subject_hierarchy):
-    """Apply hierarchical minimum samples pruning to a sample iterator."""
-    dataset = dataset_from_samples(samples_iterator)
-    dataset.subjects = prune_subject_targets_to_minimum_samples(min_samples, dataset.subjects, subject_hierarchy)
-    return samples_from_dataset(dataset)
-
-
-def _prune_min_samples(samples_iterator, min_samples):
+def _filter_min_samples(samples_iterator, min_samples):
     """Apply standard minimum samples pruning to a sample iterator."""
     dataset = dataset_from_samples(samples_iterator)
     dataset = filter_subjects_with_insufficient_samples(dataset, min_samples)
     return samples_from_dataset(dataset)
 
 
-def _prune_by_level(samples_iterator, prune_level, subject_hierarchy):
+def _prune_by_level(samples_iterator, prune_level, min_samples, subject_hierarchy):
     """Apply level-based pruning to a sample iterator."""
     if prune_level < 1:
         raise ValueError("prune level must be at least 1")
     dataset = dataset_from_samples(samples_iterator)
     dataset.subjects = prune_subject_targets_to_level(prune_level, dataset.subjects, subject_hierarchy)
-    return samples_from_dataset(dataset)
+    pruned_iterator = samples_from_dataset(dataset)
+    filtered_iterator = _filter_min_samples(pruned_iterator, min_samples)
+    return filtered_iterator
 
 
-def _default_pruning(samples_iterator, min_samples, subject_hierarchy):
+def _prune_min_samples(samples_iterator, min_samples, subject_hierarchy):
     """Combine hierarchical and standard minimum sample pruning for a samples iterator."""
-    pruned_iterator = _prune_hierarchical_min_samples(samples_iterator, min_samples, subject_hierarchy)
-    pruned_iterator = _prune_min_samples(pruned_iterator, min_samples)
-    return pruned_iterator
+    # prune hierarchy
+    dataset = dataset_from_samples(samples_iterator)
+    dataset.subjects = prune_subject_targets_to_minimum_samples(min_samples, dataset.subjects, subject_hierarchy)
+    pruned_iterator = samples_from_dataset(dataset)
+    # filter min samples
+    filtered_iterator = _filter_min_samples(pruned_iterator, min_samples)
+    return filtered_iterator
 
 
 def _load_qucosa_samples(
@@ -59,7 +57,7 @@ def _load_qucosa_samples(
     text_source: str = "titles",
     lang_code: str = None,
     langid_check: bool = False,
-    stemming: bool = False,
+    pruning_method: str = "min_samples_10",
     check_qucosa_download: bool = False,
 ) -> Iterator[Sample]:
     subject_hierarchy = qucosa_subject_hierarchy_by_subject_schema(subject_schema)
@@ -73,12 +71,17 @@ def _load_qucosa_samples(
             raise ValueError("lang code can not be None when lang check is requested")
         sample_iterator = filter_samples_by_detected_language_via_langid(sample_iterator, lang_code)
 
-    if stemming:
-        if lang_code is None:
-            raise ValueError("lang code can not be None when stemming is requested")
-        sample_iterator = apply_nltk_snowball_stemming_to_document_samples_iterator(sample_iterator, lang_code)
-
-    return _default_pruning(sample_iterator, 10, subject_hierarchy)
+    if pruning_method == "no_pruning":
+        return sample_iterator
+    if pruning_method == "filter_samples_10":
+        return _filter_min_samples(sample_iterator, 10)
+    if pruning_method == "min_samples_10":
+        return _prune_min_samples(sample_iterator, 10, subject_hierarchy)
+    if pruning_method == "level_1":
+        return _prune_by_level(sample_iterator, 1, 10, subject_hierarchy)
+    if pruning_method == "level_2":
+        return _prune_by_level(sample_iterator, 2, 10, subject_hierarchy)
+    raise RuntimeError("unknown pruning method")
 
 
 def qucosa_named_datasets_tuple_list(
@@ -95,39 +98,85 @@ def qucosa_named_datasets_tuple_list(
 
     datasets: List[Tuple[str, Callable[[], Iterator[Sample]], Callable[[], SubjectHierarchy]]] = [
         ("qucosa_all_titles_rvk",
-            lambda: _load_qucosa_samples("rvk", "titles", None, False, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "titles", None, False, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_all_titles_ddc",
-            lambda: _load_qucosa_samples("ddc", "titles", None, False, False, cqd), lazy_ddc),
+            lambda: _load_qucosa_samples(
+                "ddc", "titles", None, False, "min_samples_10", cqd
+            ), lazy_ddc),
         ("qucosa_de_titles_rvk",
-            lambda: _load_qucosa_samples("rvk", "titles", "de", False, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "titles", "de", False, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_de_titles_ddc",
-            lambda: _load_qucosa_samples("ddc", "titles", "de", False, False, cqd), lazy_ddc),
+            lambda: _load_qucosa_samples(
+                "ddc", "titles", "de", False, "min_samples_10", cqd
+            ), lazy_ddc),
         ("qucosa_de_titles_langid_rvk",
-            lambda: _load_qucosa_samples("rvk", "titles", "de", True, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "titles", "de", True, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_de_titles_langid_ddc",
-            lambda: _load_qucosa_samples("ddc", "titles", "de", True, False, cqd), lazy_ddc),
+            lambda: _load_qucosa_samples(
+                "ddc", "titles", "de", True, "min_samples_10", cqd
+            ), lazy_ddc),
         ("qucosa_de_complete_but_only_titles_rvk",
-            lambda: _load_qucosa_samples("rvk", "complete_but_only_titles", "de", False, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "complete_but_only_titles", "de", False, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_de_abstracts_rvk",
-            lambda: _load_qucosa_samples("rvk", "abstracts", "de", False, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "abstracts", "de", False, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_de_abstracts_ddc",
-            lambda: _load_qucosa_samples("ddc", "abstracts", "de", False, False, cqd), lazy_ddc),
+            lambda: _load_qucosa_samples(
+                "ddc", "abstracts", "de", False, "min_samples_10", cqd
+            ), lazy_ddc),
         ("qucosa_de_abstracts_langid_rvk",
-            lambda: _load_qucosa_samples("rvk", "abstracts", "de", True, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "abstracts", "de", True, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_de_abstracts_langid_ddc",
-            lambda: _load_qucosa_samples("ddc", "abstracts", "de", True, False, cqd), lazy_ddc),
+            lambda: _load_qucosa_samples(
+                "ddc", "abstracts", "de", True, "min_samples_10", cqd
+            ), lazy_ddc),
         ("qucosa_de_complete_but_only_abstracts_rvk",
-            lambda: _load_qucosa_samples("rvk", "complete_but_only_abstracts", "de", False, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "complete_but_only_abstracts", "de", False, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_de_fulltexts_rvk",
-            lambda: _load_qucosa_samples("rvk", "fulltexts", "de", False, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "fulltexts", "de", False, "min_samples_10", cqd
+            ), lazy_rvk),
         ("qucosa_de_fulltexts_ddc",
-            lambda: _load_qucosa_samples("ddc", "fulltexts", "de", False, False, cqd), lazy_ddc),
+            lambda: _load_qucosa_samples(
+                "ddc", "fulltexts", "de", False, "min_samples_10", cqd
+            ), lazy_ddc),
         ("qucosa_de_fulltexts_langid_rvk",
-            lambda: _load_qucosa_samples("rvk", "fulltexts", "de", True, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "fulltexts", "de", True, "min_samples_10", cqd
+            ), lazy_rvk),
+        ("qucosa_de_fulltexts_langid_rvk_level_1",
+            lambda: _load_qucosa_samples(
+                "rvk", "fulltexts", "de", True, "level_1", cqd
+            ), lazy_rvk),
+        ("qucosa_de_fulltexts_langid_rvk_level_2",
+            lambda: _load_qucosa_samples(
+                "rvk", "fulltexts", "de", True, "level_2", cqd
+            ), lazy_rvk),
+        ("qucosa_de_fulltexts_langid_rvk_no_pruning",
+            lambda: _load_qucosa_samples(
+                "rvk", "fulltexts", "de", True, "no_pruning", cqd
+            ), lazy_rvk),
         ("qucosa_de_fulltexts_langid_ddc",
-            lambda: _load_qucosa_samples("ddc", "fulltexts", "de", True, False, cqd), lazy_ddc),
+            lambda: _load_qucosa_samples(
+                "ddc", "fulltexts", "de", True, "min_samples_10", cqd
+            ), lazy_ddc),
         ("qucosa_de_complete_but_only_fulltexts_rvk",
-            lambda: _load_qucosa_samples("rvk", "complete_but_only_fulltexts", "de", False, False, cqd), lazy_rvk),
+            lambda: _load_qucosa_samples(
+                "rvk", "complete_but_only_fulltexts", "de", False, "min_samples_10", cqd
+            ), lazy_rvk),
     ]
     return datasets
 
