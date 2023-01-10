7d7c71dbcd406272dd715a5038d0399d15d9b7d4
Thomas Low <code@thomaslow.com>
2023-01-10 11:34:12 +0000

    Update default clustering models to not initialize vectorizer if not needed.

--- code/python/src/slub_docsa/experiments/qucosa/models.py
+++ code/python/src/slub_docsa/experiments/qucosa/models.py
@@ -2,6 +2,8 @@
 
 # pylint: disable=unnecessary-lambda
 
+from functools import partial
+
 from sklearn.cluster import MiniBatchKMeans, AgglomerativeClustering
 
 from slub_docsa.experiments.common.vectorizer import get_cached_tfidf_stemming_vectorizer
@@ -15,47 +17,47 @@ def default_qucosa_named_clustering_models_tuple_list(
     n_subjects: int,
 ) -> NamedClusteringModelTupleList:
     """Return default qucosa clustering models."""
-    tfidf_vectorizer_10k = get_cached_tfidf_stemming_vectorizer(max_features=10000)
-    dbmdz_bert_vectorizer_sts_1 = _get_cached_dbmdz_bert_vectorizer(subtext_samples=1)
+    tfidf_vectorizer_10k = partial(get_cached_tfidf_stemming_vectorizer, max_features=10000)
+    dbmdz_bert_vectorizer_sts_1 = partial(_get_cached_dbmdz_bert_vectorizer, subtext_samples=1)
 
     models = [
         ("random_c=20", lambda: RandomClusteringModel(n_clusters=20)),
         ("random_c=subjects", lambda: RandomClusteringModel(n_clusters=n_subjects)),
         ("tfidf_10k_kMeans_c=20", lambda: ScikitClusteringModel(
             model=MiniBatchKMeans(n_clusters=20),
-            vectorizer=tfidf_vectorizer_10k
+            vectorizer=tfidf_vectorizer_10k()
         )),
         ("tfidf_10k_kMeans_c=subjects", lambda: ScikitClusteringModel(
             model=MiniBatchKMeans(n_clusters=n_subjects),
-            vectorizer=tfidf_vectorizer_10k
+            vectorizer=tfidf_vectorizer_10k()
         )),
         ("tfidf_10k_agg_sl_cosine_c=subjects", lambda: ScikitClusteringModel(
             model=AgglomerativeClustering(n_clusters=n_subjects, affinity="cosine", linkage="single"),
-            vectorizer=tfidf_vectorizer_10k
+            vectorizer=tfidf_vectorizer_10k()
         )),
         ("tfidf_10k_agg_sl_eucl_c=subjects", lambda: ScikitClusteringModel(
             model=AgglomerativeClustering(n_clusters=n_subjects, affinity="euclidean", linkage="single"),
-            vectorizer=tfidf_vectorizer_10k
+            vectorizer=tfidf_vectorizer_10k()
         )),
         ("tfidf_10k_agg_ward_eucl_c=subjects", lambda: ScikitClusteringModel(
             model=AgglomerativeClustering(n_clusters=n_subjects, affinity="euclidean", linkage="ward"),
-            vectorizer=tfidf_vectorizer_10k
+            vectorizer=tfidf_vectorizer_10k()
         )),
         ("bert_kMeans_c=20", lambda: ScikitClusteringModel(
             model=MiniBatchKMeans(n_clusters=20),
-            vectorizer=dbmdz_bert_vectorizer_sts_1
+            vectorizer=dbmdz_bert_vectorizer_sts_1()
         )),
         ("bert_kMeans_c=subjects", lambda: ScikitClusteringModel(
             model=MiniBatchKMeans(n_clusters=n_subjects),
-            vectorizer=dbmdz_bert_vectorizer_sts_1
+            vectorizer=dbmdz_bert_vectorizer_sts_1()
         )),
         ("bert_kMeans_agg_sl_eucl_c=subjects", lambda: ScikitClusteringModel(
             model=AgglomerativeClustering(n_clusters=n_subjects, affinity="euclidean", linkage="single"),
-            vectorizer=dbmdz_bert_vectorizer_sts_1
+            vectorizer=dbmdz_bert_vectorizer_sts_1()
         )),
         ("bert_kMeans_agg_ward_eucl_c=subjects", lambda: ScikitClusteringModel(
             model=AgglomerativeClustering(n_clusters=n_subjects, affinity="euclidean", linkage="ward"),
-            vectorizer=dbmdz_bert_vectorizer_sts_1
+            vectorizer=dbmdz_bert_vectorizer_sts_1()
         )),
     ]
 
